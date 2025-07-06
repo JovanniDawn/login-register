b605fd0db3b1794f84b5c4da3ab3cc160a221fa7
JovanniDawn <jovannidawn@gmail.com>
2025-07-07 00:32:51 +0800

    with db, register already functioning

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,45 @@
+const User = require("../models/user");
+
 const test = (req, res) => {
   res.json("test is working");
 };
 
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    //Check if name was entered
+    if (!name) {
+      return res.json({
+        error: "Name is required",
+      });
+    }
+    // check is password is good
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "Password required and should be 6 characteres long",
+      });
+    }
+    // check email
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "Email is taken",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   test,
+  registerUser,
 };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const { mongoose } = require("mongoose");
 
 const app = express();
+
+// db connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database Connected"))
+  .catch(() => console.log("Database Not Connected", err));
+
+// middleware
+app.use(express.json());
+
+//mongodbpass = yHprSIGjPZdzDbOi
 
 app.use("/", require("./routes/authRoutes"));
 
